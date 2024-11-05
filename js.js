f75072be93fc6f022e9614063b5440e2effba816
VitaliyMubarakov <58411554+VitaliyMubarakov@users.noreply.github.com>
2024-11-05 22:17:04 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,20 +7,26 @@ console.log(`
 
 
 
-console.info("- StartInitWeb v 0.8");
+console.info("- StartInitWeb v 0.9");
 
 //$("#wrapper").load("./about.html body")
+let wrapper = document.getElementById("wrapper");
 
-fetch('about.html')
-    .then(response => {
-        if (!response.ok) throw new Error("Ошибка загрузки данных");
-        return response.text();
-    })
-    .then(data => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
-        const textContent = doc.body.innerText; // Извлекаем текст из <body>
-        console.log("Текст из HTML файла:", textContent);
-        console.log("Текст из файла:", data);
-    })
-    .catch(error => console.error("Ошибка:", error));
+
+async function SetHtmlAbout() {
+    await fetch('about.html')
+        .then(response => {
+            if (!response.ok) throw new Error("Ошибка загрузки данных");
+            return response.text();
+        })
+        .then(data => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, 'text/html');
+            const textContent = doc.body; // Извлекаем текст из <body>
+            console.log("Текст из HTML файла:", textContent);
+            console.log("Текст из файла:", data);
+            wrapper.innerHTML = textContent;
+        })
+        .catch(error => console.error("Ошибка:", error));
+}
+
