309b794036980c9ab6558287e37dc24646d3bf1a
VitaliyMubarakov <58411554+VitaliyMubarakov@users.noreply.github.com>
2024-11-05 21:49:09 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,12 +7,12 @@ console.log(`
 
 
 
-console.info("- StartInitWeb v 0.4");
+console.info("- StartInitWeb v 0.5");
 
 //$("#wrapper").load("./about.html body")
 
 $.get('about.html', function (data) {
-    loadedContent = $(data).find('body').html(); // Извлекаем содержимое <body> и сохраняем в переменную
+    loadedContent = $(data).html(); // Извлекаем содержимое <body> и сохраняем в переменную
     $('#wrapper').html(loadedContent); // Вставляем содержимое в #wrapper
     console.log("Загруженные данные:", loadedContent);
 });
