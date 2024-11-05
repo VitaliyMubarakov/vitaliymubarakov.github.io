322bdeb6cf0b8cd46b06abee653212eb27fa4706
VitaliyMubarakov <58411554+VitaliyMubarakov@users.noreply.github.com>
2024-11-05 22:33:17 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,16 +7,37 @@ console.log(`
 
 
 
-console.info("- StartInitWeb v 3");
+console.info("- StartInitWeb v 4");
 
 //$("#wrapper").load("./about.html body")
 let wrapper = document.getElementById("wrapper");
 
 console.info("1");
 SetHtmlAbout();
+setTimeout(SetHtmlProjects, 6000);
 
 async function SetHtmlAbout() {
     await fetch('about.html')
+        .then(response => {
+            if (!response.ok) throw new Error("Ошибка загрузки данных");
+            return response.text();
+        })
+        .then(data => {
+            console.info("2");
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, 'text/html');
+            const textContent = doc.body; // Извлекаем текст из <body>
+            console.info("3");
+            console.log("Текст из HTML файла:", textContent);
+            console.log("Текст из файла:", data);
+            console.info("4");
+            wrapper.innerHTML = data;
+        })
+        .catch(error => console.error("Ошибка:", error));
+}
+
+async function SetHtmlProjects() {
+    await fetch('projects.html')
         .then(response => {
             if (!response.ok) throw new Error("Ошибка загрузки данных");
             return response.text();
