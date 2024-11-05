0e70e06a6522b6e9a65f37601053982441f14464
VitaliyMubarakov <58411554+VitaliyMubarakov@users.noreply.github.com>
2024-11-05 22:19:25 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,12 +7,12 @@ console.log(`
 
 
 
-console.info("- StartInitWeb v 0.9");
+console.info("- StartInitWeb v 1");
 
 //$("#wrapper").load("./about.html body")
 let wrapper = document.getElementById("wrapper");
 
-
+console.info("1");
 async function SetHtmlAbout() {
     await fetch('about.html')
         .then(response => {
@@ -20,11 +20,14 @@ async function SetHtmlAbout() {
             return response.text();
         })
         .then(data => {
+            console.info("2");
             const parser = new DOMParser();
             const doc = parser.parseFromString(data, 'text/html');
             const textContent = doc.body; // Извлекаем текст из <body>
+            console.info("3");
             console.log("Текст из HTML файла:", textContent);
             console.log("Текст из файла:", data);
+            console.info("4");
             wrapper.innerHTML = textContent;
         })
         .catch(error => console.error("Ошибка:", error));
