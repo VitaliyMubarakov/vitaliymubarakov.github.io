14b6d7615ed9710c782939b93fd8579091b7a28b
VitaliyMubarakov <58411554+VitaliyMubarakov@users.noreply.github.com>
2024-11-05 23:34:26 +0800

    css

--- js.js
+++ js.js
@@ -7,7 +7,7 @@ console.log(`
 
 
 
-console.info("- StartInitWeb v 6");
+console.info("- StartInitWeb v 7");
 
 //$("#wrapper").load("./about.html body")
 let wrapper = document.getElementById("wrapper");
