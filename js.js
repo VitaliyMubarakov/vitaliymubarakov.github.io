568ad221fabed409ad96651f61d46c31cd25fea3
VitaliyMubarakov <58411554+VitaliyMubarakov@users.noreply.github.com>
2024-11-06 13:56:52 +0800

    Update js.js

--- js.js
+++ js.js
@@ -16,53 +16,84 @@ console.info("1");
 //SetHtmlAbout();
 //setTimeout(SetHtmlProjects, 6000);
 
-async function SetHtmlAbout() {
-    Hide(wrapper);
+let projectsHTML = await GetHtmlAbout();
+let homeHTML = await GetProjectAbout();
+
+console.log("Load data");
+console.log(projectsHTML);
+console.log("=====================================");
+console.log(homeHTML);
+
+async function GetHtmlAbout() {
     await fetch('about.html')
         .then(response => {
             if (!response.ok) throw new Error("Ошибка загрузки данных");
             return response.text();
         })
         .then(data => {
-            console.info("2");
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, 'text/html');
-            const textContent = doc.body; // Извлекаем текст из <body>
-            
-            wrapper.innerHTML = data;
-            
-            setTimeout(() => Show(wrapper), 50); // Задержка для запуска анимации
-            window.scrollTo(0, 0); 
-
+            projectsHTML = data;
         })
         .catch(error => console.error("Ошибка:", error));
-    
 }
 
-async function SetHtmlProjects() {
-    Hide(wrapper);
+async function GetProjectAbout() {
     await fetch('projects.html')
         .then(response => {
             if (!response.ok) throw new Error("Ошибка загрузки данных");
             return response.text();
         })
         .then(data => {
-            console.info("2");
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, 'text/html');
-            const textContent = doc.body; // Извлекаем текст из <body>
-            console.info("3");
-            console.log("Текст из HTML файла:", textContent);
-            console.log("Текст из файла:", data);
-            console.info("4");
-            wrapper.innerHTML = data;
-            eval("InitProjectJS();");
-            setTimeout(() => Show(wrapper), 50); // Задержка для запуска анимации
-            window.scrollTo(0, 0); 
-
+            projectsHTML = data;
         })
         .catch(error => console.error("Ошибка:", error));
 }
+
+// async function asdada() {
+//     Hide(wrapper);
+//     await fetch('about.html')
+//         .then(response => {
+//             if (!response.ok) throw new Error("Ошибка загрузки данных");
+//             return response.text();
+//         })
+//         .then(data => {
+//             console.info("2");
+//             const parser = new DOMParser();
+//             const doc = parser.parseFromString(data, 'text/html');
+//             const textContent = doc.body; // Извлекаем текст из <body>
+//             projectsHTML = data;
+//             wrapper.innerHTML = data;
+
+//             setTimeout(() => Show(wrapper), 50); // Задержка для запуска анимации
+//             window.scrollTo(0, 0); 
+
+//         })
+//         .catch(error => console.error("Ошибка:", error));
+// }
+
+// async function SetHtmlProjects() {
+//     Hide(wrapper);
+//     await fetch('projects.html')
+//         .then(response => {
+//             if (!response.ok) throw new Error("Ошибка загрузки данных");
+//             return response.text();
+//         })
+//         .then(data => {
+//             console.info("2");
+//             const parser = new DOMParser();
+//             const doc = parser.parseFromString(data, 'text/html');
+//             const textContent = doc.body; // Извлекаем текст из <body>
+//             console.info("3");
+//             console.log("Текст из HTML файла:", textContent);
+//             console.log("Текст из файла:", data);
+//             console.info("4");
+//             wrapper.innerHTML = data;
+//             eval("InitProjectJS();");
+//             setTimeout(() => Show(wrapper), 50); // Задержка для запуска анимации
+//             window.scrollTo(0, 0); 
+
+//         })
+//         .catch(error => console.error("Ошибка:", error));
+// }
 
 function Hide(e) {
     e.classList.remove("visible");
